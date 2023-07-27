0ec0359e58f70543560fb16c8c878ad4938c8f9e
Mirsaid04 <mirsaidd04@gmail.com>
2023-07-27 11:31:50 +0900

    C# Final Project

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main()
         {
-           const decimal goldvalue1ginDollar=1983; //USD $
+            string yesOrno=(""); 
+            do{
+                        const decimal goldvalue1ginDollar=1983; //USD $
             Console.WriteLine("\nWelcome to exchanging program in .Net framework");
             Console.WriteLine("What do you have: \n1.Gold\n2.Dollor");
             int choosednumber= int.Parse(Console.ReadLine()!);
@@ -14,20 +16,22 @@
                 case 1: 
                     Console.WriteLine("How much do you have");
                     decimal goldOfyou= Convert.ToDecimal(Console.ReadLine());
-                    Console.WriteLine($"Your gold is {goldOfyou*goldvalue1ginDollar}$ in USA dollar");break;
+                    Console.WriteLine($"Your gold is {goldOfyou*goldvalue1ginDollar}$ in USA dollar");
+                    break;
                 
                 case 2:
                     Console.WriteLine("How much do you have");
                     decimal dollorOfyou= Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine($"Your gold is {dollorOfyou/goldvalue1ginDollar}g in gram of Gold");break;
-            }
-            Console.WriteLine("If you have Gold and do you want to change it to Korean won?\nLet's gooo--->\n ");
-            Console.Write("How much Gold do you have in gram :");
-            decimal goldQuantity=Convert.ToDecimal(Console.ReadLine());
-            decimal koreanwon1gGold=2516264;
+                    Console.WriteLine($"Your gold is {dollorOfyou/goldvalue1ginDollar}g in gram of Gold");
+                    break;
 
-            Console.WriteLine($"Your gold is {goldQuantity*koreanwon1gGold} in Korean won");
-            Console.WriteLine("<--Thank you for your attention-->");
+                default:
+                    Console.WriteLine("Sorry, you entered invalid input,\nTry again later");
+                    break;
+            }
+            Console.Write("Do you want to continue [y/n]: "); 
+            yesOrno=Console.ReadLine()!; 
+            }while(yesOrno=="y");
         }
     }
 }
